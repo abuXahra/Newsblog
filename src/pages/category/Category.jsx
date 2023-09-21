c7fd374314919ca0789f4ac6f8b56ba0003e7a61
abuXahra <abdulmuminisah79@gmail.com>
2023-09-21 21:25:44 +0100

    professioneal blog

---
+++ src/pages/category/Category.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Category = () => {
+    return (
+        <CategoryWraper>
+            <CategoryContent>
+                
+            </CategoryContent>
+        </CategoryWraper>
+    );
+}
+
+export default Category;
