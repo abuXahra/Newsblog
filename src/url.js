6b83015f298f667d4be9f5c81f810a5bd81bb472
abuXahra <abdulmuminisah@yahoo.com>
2024-01-09 11:48:20 +0100

    updated

--- src/url.js
+++
@@ -1,8 +0,0 @@
-
-// for production:
-// export const URL = "https://newsblogg.vercel.app";
-// export const IF = "https://newsblogg.vercel.app/images"
-
-// for localhost:
-export const URL = "http://localhost:5000";
-export const IF = "http://localhost:5000/images/"
